e66933f49574d051e41bdb5d3c4e101107078b0f
Sergio Gimeno <gimeno.sergio@gmail.com>
2015-05-29 13:06:49 +0200

    split code into common chunks

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-var adpure = require('./modules/popup');
+var utils = require('./modules/utils');
+
+document.addEventListener('DOMContentLoaded', function() {
+  utils.getCurrentTabUrl(function(url) {
+    console.log('GOT URL ' + url);
+    // Put the image URL in Google search.
+    utils.renderStatus('Performing Google Image search for ' + url);
+
+    utils.getImageUrl(url, function(imageUrl, width, height) {
+
+      utils.renderStatus('Search term: ' + url + '\n' +
+          'Google image search result: ' + imageUrl);
+      var imageResult = document.getElementById('image-result');
+      // Explicitly set the width/height to minimize the number of reflows. For
+      // a single image, this does not matter, but if you're going to embed
+      // multiple external images in your page, then the absence of width/height
+      // attributes causes the popup to resize multiple times.
+      imageResult.width = width;
+      imageResult.height = height;
+      imageResult.src = imageUrl;
+      imageResult.hidden = false;
+
+    }, function(errorMessage) {
+      utils.renderStatus('Cannot display image. ' + errorMessage);
+    });
+  });
+});
