43e0bfea7ce4f5efcfcb5cfbe0b785649ff25613
Sergio Gimeno <sergio@MacBook-Pro-de-Sergio.local>
2015-11-15 23:58:59 +0100

    remove browserify and popup code

--- README.md
+++ README.md
@@ -5,26 +5,17 @@ IPFS assets cache Chrome Extension
 
 ```
 >npm install
->source env.sh
 ```
-
 
 ####Gulp tasks
 
  + `clean`: deletes the `dist` directory
- + `build`: builds the app using `js` and `html` tasks
-  + `js`: browserify bundle
-  + `html`: copy HTML files to `dist`
-  + `assets`: copies icon.png and manifest.json `dist`
- + `watch`: watch HTML & JS files with watchify
+ + `assets`: copies files to `dist`
+ + `watch`: watch for source changes
 
+`source env.sh` for using local gulp alias
 
 ####TODO:
 
  + Add tests
- + Improve gulp build code
-
-####References
-
- + [browserify](http://browserify.org/)
- + [Gulp + browserify](http://viget.com/extend/gulp-browserify-starter-faq)
+ + Improve gulpfile
--- gulpfile.js
+++ gulpfile.js
@@ -1,95 +1,12 @@
 'use strict';
 
 var gulp = require('gulp');
-var rename = require('gulp-rename');
-var util = require('gulp-util');
 var del = require('del');
-var browserify = require('browserify');
-var source = require('vinyl-source-stream');
-var watchify = require('watchify');
-var buffer = require('vinyl-buffer');
-var sourcemaps = require('gulp-sourcemaps');
-var assign = require('lodash.assign');
 var livereload = require('gulp-livereload');
 
-/*
-	A more sophisticated browserify build & watch
-	https://github.com/gulpjs/gulp/blob/master/docs/recipes/fast-browserify-builds-with-watchify.md
- */
-
-// add custom browserify options here
-var customOpts = {
-  entries: ['./src/index.js'],
-  debug: true
-};
-
-
-// you can run `gulp js` to build the file
-gulp.task('js', function(){
-  var opts = assign({}, browserify.args, customOpts);
-  var b = browserify(opts);
-
-  // add transformations here
-  // i.e. b.transform(coffeeify);
-
-  //TODO: reuse bundle function for watchify and browserify
-  function bundle() {
-    return b.bundle()
-      // log errors if they happen
-      .on('error', util.log.bind(util, 'Browserify Error'))
-      .pipe(source('bundle.js'))
-      // optional, remove if you don't need to buffer file contents
-      .pipe(buffer())
-      // optional, remove if you dont want sourcemaps
-      //.pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
-         // Add transformation tasks to the pipeline here.
-      .pipe(sourcemaps.write('./')) // writes .map file
-      .pipe(gulp.dest('./dist'))
-      .pipe(livereload());
-  }
-
-  return bundle();
-
-});
-
-gulp.task('watchify', function(){
-  var opts = assign({}, watchify.args, customOpts);
-  var b = watchify(browserify(opts));
-
-  // add transformations here
-  // i.e. b.transform(coffeeify);
-
-  //TODO: reuse bundle function for watchify and browserify
-  function bundle() {
-    return b.bundle()
-      // log errors if they happen
-      .on('error', util.log.bind(util, 'Browserify Error'))
-      .pipe(source('bundle.js'))
-      // optional, remove if you don't need to buffer file contents
-      .pipe(buffer())
-      // optional, remove if you dont want sourcemaps
-      .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
-         // Add transformation tasks to the pipeline here.
-      .pipe(sourcemaps.write('./')) // writes .map file
-      .pipe(gulp.dest('./dist'))
-      .pipe(livereload());
-  }
-
-  b.on('update', bundle); // on any dep update, runs the bundler
-  b.on('log', util.log); // output build logs to terminal
-
-  return bundle();
-});
-
-gulp.task('html', function(){
-	gulp.src(['./src/index.html'])
-    .pipe(rename('popup.html'))
-		.pipe(gulp.dest('./dist'))
-		.pipe(livereload());
-});
 
 gulp.task('assets', function(){
-	gulp.src(['./src/manifest.json', './src/icon.png'])
+	gulp.src(['./src/**/*.{json,png,js}'])
 		.pipe(gulp.dest('./dist'))
 		.pipe(livereload());
 });
@@ -98,10 +15,9 @@ gulp.task('clean', function(done) {
   del(['./dist'], done);
 });
 
-gulp.task('watch', ['watchify'], function() {
+gulp.task('watch', function() {
     livereload.listen();
-    gulp.watch(['./src/**/*.html'], ['html']);
-    gulp.watch(['./src/manifest.json', './src/icon.png'], ['assets'])
+    gulp.watch(['./src/**/*.{json,png,js}'], ['assets'])
 });
 
-gulp.task('build', ['assets', 'html', 'js']);
+gulp.task('build', ['assets']);
